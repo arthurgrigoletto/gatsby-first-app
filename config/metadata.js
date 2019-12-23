9d29f969a12507a22cd06853e85ab86442b4d797
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-12-23 09:58:19 -0300

    Initial Commit

---
+++ config/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+  title: `Arthur Grigoletto`,
+  siteUrl: `https://www.arthurgrigoletto.com.br`,
+  description: `My First Gatsby App`,
+  social: [
+    {
+      name: 'github',
+      url: 'https://github.com/arthurgrigoletto'
+    }
+  ],
+}
