0ceaba5b5aadc5f70c43bacb295d984cf1e72b2f
Arthur Grigoletto <arthur.grigoletto.lima@gmail.com>
2019-12-23 09:57:58 -0300

    Initial Commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,6 +4,56 @@
  * See: https://www.gatsbyjs.org/docs/gatsby-config/
  */
 
+const siteMetadata = require('./config/metadata');
+
 module.exports = {
-  /* Your site config here */
+  siteMetadata,
+  plugins: [
+    `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `ArthurGrigoletto`,
+        short_name: `ArthurG`,
+        start_url: `/`,
+        background_color: `#fff`,
+        theme_color: `#7159c1`,
+        display: `standalone`,
+        icon: `src/assets/icon.png`
+      },
+    },
+    {
+      resolve: `gatsby-plugin-nprogress`,
+      options: {
+        color: '#7159c1',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-86672935-1",
+      },
+    },
+    {
+      resolve: `gatsby-plugin-canonical-urls`,
+      options: {
+        siteUrl: `https://www.arthurgrigoletto.com.br`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-styled-components`,
+      options: {
+        displayName: process.env.NODE_ENV !== 'production',
+      },
+    },
+    {
+      resolve: `gatsby-plugin-page-creator`,
+      options: {
+        path: `${__dirname}/src/pages`,
+        ignore: [`**/styles.js`],
+      },
+    },
+    `gatsby-plugin-offline`
+  ]
 }
